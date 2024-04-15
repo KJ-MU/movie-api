c5c52cd54cd31aed41c53fa3d9d647596eefbfb7
zainabyahya <zainabh_2015@yahoo.com>
2024-04-15 17:54:07 +0300

    add slider template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import "./slider.css";
 import Movie from './components/Movie';
+import Carousel from './components/Carousel';
+import { useSelector, useDispatch, useEffect } from "react";
+
 
 function App() {
+  const movies = useSelector((state) => state.movies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMovies()); // Dispatch the action to fetch products
+  }, [dispatch]);
+
   return (
     <div className="App">
+      <Carousel slides={SLIDES} options={OPTIONS} />
+
       <Movie />
     </div>
   );
